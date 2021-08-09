5268e4398d07ac87197517e263b074d71a5b1780
Simon G <sghyseli@students.cs.ubc.ca>
2021-08-08 17:21:37 -0700

    Fixed some bugs and added visual histogram to security viewer

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -13,34 +13,6 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-//        Calendar date1 = Calendar.getInstance();
-//        Calendar date2 = Calendar.getInstance();
-//        Calendar date3 = Calendar.getInstance();
-//        Transaction buyBNS1;
-//        Transaction buyBNS2;
-//        Transaction sellBNS;
-//        Security bns;
-//        bns = new Security("BNS");
-//        date1.set(2019, Calendar.NOVEMBER, 20);
-//        date2.set(2020, Calendar.JUNE, 5);
-//        date3.set(2021, Calendar.MARCH, 20);
-//        buyBNS1 = new Transaction("BNS", date1, false, 1089.18,
-//                false, 0, 10, 4.99);
-//        sellBNS = new Transaction("BNS", date2, true, 420.20,
-//                false, 0, 5, 4.99);
-//        buyBNS2 = new Transaction("BNS", date3, false, 1850.10,
-//                false, 0, 20, 4.99);
-//        bns.addTransaction(buyBNS1);
-//        bns.addTransaction(buyBNS2);
-//        bns.addTransaction(sellBNS);
-//
-//        JFrame testFrame = new JFrame();
-//        testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        TransactionHistogram graph = new TransactionHistogram(bns);
-//
-//        testFrame.getContentPane().add(graph);
-//        testFrame.pack();
-//        testFrame.setVisible(true);
 
         new PortfolioSelectionPanel();
     }
